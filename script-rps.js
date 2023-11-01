0065d97c496f10e4b71917175e32087c08d380e5
Thomas One <tommyone99@gmail.com>
2023-10-31 20:42:01 -0400

    add divs and replace all console.log commands to DOM methods

--- script-rps.js
+++ script-rps.js
@@ -1,3 +1,6 @@
+const announcements = document.querySelector("#announcements");
+const roundDecision = document.querySelector("#roundDecision");
+
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
@@ -10,11 +13,6 @@ rock.addEventListener("click", getTally);
 paper.addEventListener("click", getTally);
 scissors.addEventListener("click", getTally);
 
-
-
-
-
-
 function getTally(e) {
     roundResult = playRound(e);
     if (roundResult === "WIN"){
@@ -23,15 +21,15 @@ function getTally(e) {
     else if (roundResult === "LOSS"){
         computerScore++;
     }
-    console.log(`${roundResult} PLAYER SCORE: ${playerScore} COMPUTER SCORE: ${computerScore}`);
+    announcements.textContent = `${roundResult} PLAYER SCORE: ${playerScore} COMPUTER SCORE: ${computerScore}`;
 
     if (playerScore > 4){
-        console.log("You Win! Congratulations, you are better than a computer");
+        announcements.textContent = "You Win! Congratulations, you are better than a computer";
         playerScore = 0;
         computerScore = 0;
     }
     else if (computerScore > 4){
-        console.log("You Lose! Humanity was no match against Machine!");
+        announcements.textContent = "You Lose! Humanity was no match against Machine!";
         playerScore = 0;
         computerScore = 0;
     };
@@ -54,50 +52,55 @@ function getComputerChoice() {
     return move;
 }
 
+function announceDecision(selection, computerSelection) {
+    roundDecision.textContent = `Your move: ${selection} "Computer's move: ${computerSelection}`;
+
+};
+
 function playRound(e) {
 
     selection = e.target.id.toUpperCase()
     computerSelection = getComputerChoice();
-    
+
     if (selection === "ROCK"){
         if (computerSelection === "ROCK"){
-            console.log("Draw! What a rocky resolution");
+            announceDecision(selection, computerSelection);
             return "DRAW";
         }
         else if (computerSelection === "PAPER"){
-            console.log("You Lose! Paper beats Rock");
+            announceDecision(selection, computerSelection);
             return "LOSS";
         }
         else{
-            console.log("You Win! Rock beats Scissors");
+            announceDecision(selection, computerSelection);
             return "WIN";
         }
     }  
     else if (selection === "PAPER"){
         if (computerSelection === "ROCK"){
-            console.log("You Win! Paper beats Rock");
+            announceDecision(selection, computerSelection);
             return "WIN";
         }
         else if (computerSelection === "PAPER"){
-            console.log("Draw! What a waste of Paper...");
+            announceDecision(selection, computerSelection);
             return "DRAW";
         }
         else {
-            console.log("You Lose! Scissors beat Paper");
+            announceDecision(selection, computerSelection);
             return "LOSS";
         }
     }
     else {
         if (computerSelection === "ROCK"){
-            console.log("You Lose! Rock beats Scissors");
+            announceDecision(selection, computerSelection);
             return "LOSS";
         }
         else if (computerSelection === "PAPER"){
-            console.log("You Win! Scissors beats Paper");
+            announceDecision(selection, computerSelection);
             return "WIN";
         }
         else {
-            console.log("Draw! 'snip snip' - Edward Scissorhands");
+            announceDecision(selection, computerSelection);
             return "DRAW";
         }
     }
